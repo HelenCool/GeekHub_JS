aa60759a76c1d9f07757919d316d20eb7dbbf049
Alyona Bochkaryova <thehelencool@gmail.com>
2017-10-17 18:59:04 +0300

    recursion

--- first/scripts/recursion.js
+++ first/scripts/recursion.js
@@ -1,8 +1,44 @@
 /* рекурсивно раскладывает двухмерный массив в одномерный */
 
-var first = ["a", "b", "c"];
-var second = ["d", "e", "f"];
-var third = ["g", "h", "i"];
+/*function write(n) {
+    if (n >= 0) {
+        console.log(n);
+        write(n - 1);
+    }
+}
 
-var alph = [first, second, third];
-console.log(alph);
+write(10);*/
+
+var evenNumbers = new Array();
+var k = 0;
+var oddNumbers = new Array();
+var n = 0;
+
+var data = new Array("car", "plane", "bus", "ship");
+
+for(var i = 1; i < 10; i++){
+    if( i % 2 == 0){
+        evenNumbers[k] = i;
+        k++;
+    }
+}
+
+for(var i = 1; i < 9; i++){
+    if( i % 2 != 0){
+        oddNumbers[n] = i;
+        n++;
+    }
+}
+
+var twoDimens = new Array(evenNumbers, oddNumbers, data);
+console.log(twoDimens);
+
+for(var i = 0; i < twoDimens.length; i++){
+    for(var j = 0; j < twoDimens[i].length; j++){
+        console.log("Элемент с индексом " + i + " " + j + "  равен: " + twoDimens[i][j]);
+    }
+};
+
+function toOnDementional(){
+
+}
