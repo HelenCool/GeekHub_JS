197fc5f5857cda4b2544d7fda351bcb8c921bd35
Alyona Bochkaryova <thehelencool@gmail.com>
2017-10-19 02:32:27 +0300

    Home assignement (Th, 10/19)

--- first/scripts/recursion.js
+++ first/scripts/recursion.js
@@ -1,49 +1,10 @@
-/* рекурсивно раскладывает двухмерный массив в одномерный */
-
-/*function write(n) {
-    if (n >= 0) {
-        console.log(n);
-        write(n - 1);
+function count(number) {
+    if (number === 0) {
+        console.log("Something goes wrong");
+    } else {
+        var newNumber = number - 1;
+        console.log("Hey," + newNumber);
+        count(newNumber);
     }
 }
-
-write(10);*/
-
-var evenNumbers = [];
-var k = 0; // индексы массива evenNumbers
-var oddNumbers = [];
-var n = 0;// индексы массива oddNumbers
-
-var data = ["car", "plane", "bus", "ship"];
-
-for(var i = 1; i < 10; i++){
-    if( i % 2 == 0){
-        evenNumbers[k] = i;
-        k++;
-    }
-}
-for(var i = 1; i < 9; i++){
-    if( i % 2 != 0){
-        oddNumbers[n] = i;
-        n++;
-    }
-}
-
-var twoDimens = [evenNumbers, oddNumbers, data];
-console.log(twoDimens);
-
-/*for(var i = 0; i < twoDimens.length; i++){
-    for(var j = 0; j < twoDimens[i].length; j++){
-        console.log("Элемент с индексом " + i + " " + j + "  равен: " + twoDimens[i][j]);
-    }
-};*/
-
-var toOneDimentional = function(twoDimens.prototype.length){
-  var result = [];
-  var a=0; //индексы массива result
-    for(var b = 0; b < result.length; b++){
-          result[a]=twoDimens[i][j];
-      }
-};
-
-console.log(toOneDimentional());
+count(8);
