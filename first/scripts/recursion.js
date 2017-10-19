2c03d6d3f6829059ae21e8a0a488ee3a23c61906
Alyona Bochkaryova <thehelencool@gmail.com>
2017-10-19 15:57:38 +0300

    Home assignement (Th, 10/19)

--- first/scripts/recursion.js
+++ first/scripts/recursion.js
@@ -8,12 +8,3 @@ function count(number) {
     }
 }
 count(8);
-=======
-/* рекурсивно раскладывает двухмерный массив в одномерный */
-
-var first = ["a", "b", "c"];
-var second = ["d", "e", "f"];
-var third = ["g", "h", "i"];
-
-var alph = [first, second, third];
-console.log(alph);
