1921b33dc6bc415952e7b45442ed6dee5d91d90b
Alyona Bochkaryova <thehelencool@gmail.com>
2017-10-21 21:09:17 +0300

    Corrected H/w

--- first/scripts/benchmarking.js
+++ first/scripts/benchmarking.js
@@ -14,15 +14,16 @@ for (var j=0;j<100000-1;j++){
 
 //определение объекта с рандомными строками
 for (var j =0; j<100000-1;j++){
-    object2[j]= Math.random().toString();
+    object2[j]= Math.random().toString(36);
 }
+
 
 //функция for для упорядоченого массива строк
 var forArray = function() {
     for(var i=0;i<chars.length;i++) {
         chars[i]="new char = " + String.fromCharCode(j+1);
     }
-    console.log(chars);
+    //console.log(chars);
 }
 
 //функция for in для упорядоченого массива строк
@@ -30,53 +31,73 @@ var forinArray = function(){
     for (var key in chars){
         chars[key]="new char = " + String.fromCharCode(j+1);
     }
-    console.log(chars);
+    //console.log(chars);
 }
 
 //функция for для упорядоченого объекта строк
-var forObject1 = function() {
+var forObject1 = function(i) {
+    var key = Object.keys(this)[i]; // возвращает массив из свойств переданного объекта
     for(var i=0;i<object1.length;i++) {
-        object1[i]="new obj = " + String.fromCharCode(j+1);
+        object1[key]="new obj = " + String.fromCharCode(j+1);
     }
-    console.log(object1);
+    //console.log(object1);
 }
 
+var f = forObject1.bind(object1); //привязка контекста
+
 //функция for in для упорядоченого объекта строк
-var forinObject1 = function(){
+var forinObject1 = function(i){
     for (var key in object1){
         object1[key]= "new obj = " + String.fromCharCode(j+1);
     }
-    console.log(object1);
+    //console.log(object1);
 }
 
+
 //функция for для объекта с рандомными строками
-var forObject2 = function() {
+var forObject2 = function(i) {
+    var key = Object.keys(this)[i]; // возвращает массив из свойств
     for(var i=0;i<object2.length;i++) {
-        object2[i]= (Math.random()*100).toString();
+        object2[key]= (Math.random()*100).toString();
     }
-    console.log(object2);
+    //console.log(object2);
 }
+
+var g = forObject2.bind(object2); //привязка контекста
 
 //функция for in для объекта с рандомными строками
 var forinObject2 = function(){
     for (var key in object2){
         object2[key]= (Math.random()*100).toString();
     }
-    console.log(object2);
+    //console.log(object2);
 }
+
 
 //бенчмарки
 console.time("forArray");
+forArray();
 console.timeEnd("forArray");
+
 console.time("forinArray");
+forinArray();
 console.timeEnd("forinArray");
 
+
 console.time("forObject1");
+f();
+forObject1();
 console.timeEnd("forObject1");
+
 console.time("forinObject1");
+forinObject1();
 console.timeEnd("forinObject1");
 
+
 console.time("forObject2");
+g();
 console.timeEnd("forObject2");
+
 console.time("forinObject2");
+forinObject2();
 console.timeEnd("forinObject2");
